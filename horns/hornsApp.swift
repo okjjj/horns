9961ec99a0b69c94413ae193d7618de6f4ff2010
okjjj <19897585+okjjj@users.noreply.github.com>
2024-10-29 17:51:29 -0400

    Initial Commit

---
+++ horns/hornsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hornsApp.swift
+//  horns
+//
+//  Created by current on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct hornsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hornsTests/hornsTests.swift
@@ -0,0 +1,17 @@
+//
+//  hornsTests.swift
+//  hornsTests
+//
+//  Created by current on 10/29/24.
+//
+
+import Testing
+@testable import horns
+
+struct hornsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
