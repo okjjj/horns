4e5c2d20559b3a4f1fe6d5c60d074664958b43bd
okjjj <19897585+okjjj@users.noreply.github.com>
2024-11-03 14:30:18 -0500

    hi

--- horns/ContentView.swift
+++ horns/ContentView.swift
@@ -1,23 +1,55 @@
-//
-//  ContentView.swift
-//  horns
-//
-//  Created by current on 10/29/24.
-//
-
 import SwiftUI
+import AVFoundation
+
+
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State var player: AVAudioPlayer!
+    mutating func playAudio(horns: String) {
+        guard let path = (((Bundle.main.path(forResource: horns, ofType: "mp3")) ))  else {
+                print("path not created")
+                return
+            }
+        let url = URL(fileURLWithPath: path)
+            
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                player?.volume = 0.9
+                player?.play()
+            }
+            catch {
+                        print("error?")
+                    }
         }
-        .padding()
-    }
+
+    var body: some View {
+//       VStack {
+//           Image(systemName: "horn")
+//              .imageScale(.large)
+//              .foregroundStyle(.tint)
+//        }
+        Button(action: {
+//                _ = Bundle.main.path(forResource: "horns", ofType: "mp3")
+                // let audioFileURL = URL(fileURLWithPath: "/Users/current/Downloads/horns.mp3")
+                 // let audioPlayer = try! AVAudioPlayer(contentsOf: url)
+                player.play()
+                }
+            )
+            {
+                Text("Play")
+                .buttonStyle(.bordered)
+                .controlSize(.extraLarge)
+                .buttonBorderShape(.capsule)
+                // .tint(.pink)
+            }
+            .padding()
+        }
+        
 }
+
+
+
 
 #Preview {
     ContentView()
